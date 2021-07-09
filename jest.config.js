aa450f58810ce9d7882b363788bcbe5afcc3f487
amenac <chakroun.amenallah@gmail.com>
2021-07-09 14:31:56 +0200

    init

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  projects: ['<rootDir>/apps/alphaproject'],
+  projects: ['<rootDir>/apps/alphaproject', '<rootDir>/libs/ui'],
 };
---
+++ libs/ui/src/lib/ui.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { XyzComponent } from './xyz/xyz.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [
+    XyzComponent
+  ],
+})
+export class UiModule {}
